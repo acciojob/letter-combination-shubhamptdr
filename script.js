5574ffda68b0f49dfbd36a014b25782868e33c6e
Shubham Patidar <117558233+shubhamptdr@users.noreply.github.com>
2023-04-15 11:10:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 function letterCombinations(input_digit) {
-  //Complete the function
+    if (!input_digit) {
+    return [];
+  }
+  const mapping = {
+    '2': 'abc', 
+    '3': 'def', 
+    '4': 'ghi', 
+    '5': 'jkl',
+    '6': 'mno', 
+    '7': 'pqrs', 
+    '8': 'tuv', 
+    '9': 'wxyz'
+  };
+  const output = [];
+  function backtrack(combination, remainingDigits) {
+    if (!remainingDigits) {
+      output.push(combination);
+    } else {
+      const letters = mapping[remainingDigits[0]];
+      for (let i = 0; i < letters.length; i++) {
+        const letter = letters[i];
+        backtrack(combination + letter, remainingDigits.slice(1));
+      }
+    }
+  }
+  backtrack('', input_digit);
+  return output;
 }
 
 module.exports = letterCombinations;
